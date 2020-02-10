c75ce69450c2cbb5845e51b39ddd490062732a16
Rio Karim <rio@2132.io>
2020-02-10 11:30:21 +0000

    <User> Authentication JWT & Passport

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,36 @@
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  ValidationPipe,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from './user.entity';
+import { GetUser } from './get-user.decorator';
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('/signup')
+  signUp(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<void> {
+    return this.authService.signUp(authCredentialsDto);
+  }
+
+  @Post('/signin')
+  signIn(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    return this.authService.signIn(authCredentialsDto);
+  }
+
+  @Get('/me')
+  @UseGuards(AuthGuard())
+  me(@GetUser() user: User) {
+    console.log(user);
+  }
+}
